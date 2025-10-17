5a5d3bb3860dafae24afad4d974a1e6287dc7c6b
s1140322-ops <s1140322@dmhs.kh.edu.tw>
2025-10-17 16:19:30 +0800

    hw2

---
+++ hw2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+    
+int main() {
+    int age = 16;
+    double heidht = 170.5;
+    std::string name = "dada";
+    bool is_pass = true;
+    std::cout << "姓名: " << name << "身高: " << heidht << std::endl;
+    std::cout << "年齡: " << age << "是否及格: " << is_pass <<std::endl;
+    return 0;
+}
