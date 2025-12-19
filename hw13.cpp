a4f4aa3b39519a94b015adabb7652b11373c8b68
s1140322-ops <s1140322@dmhs.kh.edu.tw>
2025-12-19 16:10:31 +0800

    hw13

---
+++ hw13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int score[3] = {95, 68, 42};
+    int temp;
+    
+    temp = score[2];
+    score[2] = score[0];
+    score[0] = temp;
+    for (int i=0;i<3;i++) {
+        std::cout << score[i] << " ";
+    }
+
+    return 0;
+}
